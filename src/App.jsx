d0acee9af9fac6cd58763ba2042fc185e649f5ee
FatimaVO <fatima.vazquez.fv.fv@gmail.com>
2022-07-03 21:17:59 -0500

    indentation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [data, setData]= useState({})
+  const [data, setData] = useState({})
   const [temperature, setTemperature] = useState(0)
-  const [isCelcius, setIsCelcius]=useState(true)
+  const [isCelcius, setIsCelcius] = useState(true)
 
   useEffect(() => {
     const success = pos => {
@@ -13,51 +13,49 @@ function App() {
       const lon = pos.coords.longitude;
       axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5b64fa2bf1a755b3b5d1c1e62820608d&units=metric`)
 
-      .then((res) => {
-        setData(res.data)
-        setTemperature((res.data.main.temp).toFixed())
-      })
+        .then((res) => {
+          setData(res.data)
+          setTemperature((res.data.main.temp).toFixed())
+        })
 
     }
 
 
     navigator.geolocation.getCurrentPosition(success);
-  },[])
-  
+  }, [])
+
   console.log(data);
   const idIcon = data.weather?.[0].icon;
 
   const convertTemperature = () => {
-    if(isCelcius){
-    setTemperature((temperature* 1.8 + 32).toFixed())
-    setIsCelcius(false)
-    }else{
-        setTemperature(((temperature - 32)/1.8).toFixed()) 
-        setIsCelcius(true)
+    if (isCelcius) {
+      setTemperature((temperature * 1.8 + 32).toFixed())
+      setIsCelcius(false)
+    } else {
+      setTemperature(((temperature - 32) / 1.8).toFixed())
+      setIsCelcius(true)
     }
-
-
-}
+  }
 
   return (
     <div className="App">
-      <div className= "weatherCard">
+      <div className="weatherCard">
         <div className="weatherConditions">
           <div className="clouds">
-          <i className="fa-solid fa-cloud"></i>{data.clouds?.all} % 
+            <i className="fa-solid fa-cloud"></i>{data.clouds?.all} %
           </div>
           <div className="wind">
-          <i className="fa-solid fa-wind"></i>{data.wind?.speed} m/s
+            <i className="fa-solid fa-wind"></i>{data.wind?.speed} m/s
           </div>
           <div className="humidity">
-          <i className="fa-solid fa-droplet"></i>{data.main?.humidity} %
+            <i className="fa-solid fa-droplet"></i>{data.main?.humidity} %
           </div>
         </div>
         <div className="weatherInformation">
-            <h1>{temperature} {isCelcius ? "°C" : "°F"}</h1>
-            <img src={`http://openweathermap.org/img/wn/${idIcon}@4x.png`} alt="" />
-            <p className="description"> {data.weather?.[0]?.description}</p>
-            <h5> <i className="fa-solid fa-location-crosshairs"></i> {data?.name}, {data.sys?.country}</h5>
+          <h1>{temperature} {isCelcius ? "°C" : "°F"}</h1>
+          <img src={`http://openweathermap.org/img/wn/${idIcon}@4x.png`} alt="" />
+          <p className="description"> {data.weather?.[0]?.description}</p>
+          <h5> <i className="fa-solid fa-location-crosshairs"></i> {data?.name}, {data.sys?.country}</h5>
         </div>
         <button onClick={convertTemperature}> °C | °F</button>
       </div>
